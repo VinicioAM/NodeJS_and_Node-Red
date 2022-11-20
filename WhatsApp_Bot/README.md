ac7692faec9eaa7489380877c23ff505fa908d6b
VinicioAM <109367504+VinicioAM@users.noreply.github.com>
2022-11-20 13:15:47 -0300

    WhatsApp bot created

---
+++ WhatsApp_Bot/README.md
@@ -0,0 +1,75 @@
+# WHATSAPP BOT
+
+To improve communication, I created a whatsapp bot that sends automatic messages. The idea is integrate the bot with anothers scripts and get automatic informations from machines and robots, in a industrial environment (ex: a failure in a welding robot or the production report of the last shift).
+
+As you can see <a href="https://github.com/VinicioAM/NodeJS_and_Node-Red/tree/main/Parameter%20Check%20Automation" target="_blank" rel="noreferrer"> **here**</a> and <a href="https://github.com/VinicioAM/IIoT/tree/main/Production_and_Scrap-Report" target="_blank" rel="noreferrer"> **here**</a>, I've created some scripts that collect data from PLCs/robots and all industrial machinery. Anyway, it must be delivered fast to managers. For this reason, I choose whatsapp instead e-mail, since we use whatsapp more often than e-mails. Telegram could be used too, but the managers preffer whatsapp.
+There are a lot of APIs/ready-to-use whatsapp bots and anothers tools to make the process easier, avoiding the creating of my own whatsapp bot. Anyway, all free features are limited-use and can not handle my reports. 
+
+
+
+<br />
+
+## OPERATING MODE
+First, I made the code so that for each message to be sent, the bot would initiate the connection, authenticate, send the message and finally close the application. This method was not effective because there was a lot of issues related to authentication with the WhatsApp server.
+The solution was make a single connection, a single authentication and send message in loop by reading a .txt file which contains the message to be sent.
+
+<br />
+
+
+## THE CODE
+The code is divided in 3 steps:
+<br />**First:** a function (datefull) to get current datetime and formating.
+<br />**Second:** a function (msg) to read a txt file where messages are stored, show below:
+
+<p align="center">
+<img src="./imgs/01.PNG" width=100% height=50% align="center">
+  <br />Read/Write TXT file function.
+</p>
+<br /><br />
+
+The function msg() read every first line of a text file and return it to an array with 2 elements only: the final_text[0] contains all contact numbers to send through WhatsApp and final_text[1] contain the text to be sent. Note that what divide numbers from text is a space ' ' (line 60 and 61).
+
+**Third:**  a function (wpp) to send the message:
+<p align="center">
+<img src="./imgs/02.PNG" width=90% height=40% align="center">
+  <br />Wpp() function - arguments.
+</p>
+
+The code starts with some arguments to be provided: path to file, filename, delay between messages, delay between file reading and max tries in case of error.
+<br /><br /><br /><br />
+
+And then:
+<p align="center">
+<img src="./imgs/03.PNG" width=90% height=50% align="center">
+    <br />Wpp() function - replace text.
+</p>
+
+
+Note that the contact numbers must be separate with ',' (line 104) while in text to be sent, 'xxx' means a breakline (line 107) and '_' means space (line 109).
+
+
+This way, adding line 60, 61, 104, 107 and 109, to script recognize contact numbers and message to send, the text file must follow this format:
+<br />553199998888,553199997777 Text:xxxThe_triple_x_is_a_breakline,_while_underscore_is_a_space._There_is_a_space_between_contacts_and_text.
+
+
+## HOW I'M USING
+The bot can be used to send any automated message. Particularly, I'm using with Node-Red (nodeJS based application). Node-Red starts the bot and keep writing the file with messages. The scope of the messages I'm using you can find here.
+
+<p align="center">
+<img src="./imgs/04.PNG" width=40% height=5% align="center">
+    <br />Exec node to start the bot.
+</p>
+<br /><br />
+The exec node starts the bot. It is trigged everytime the Node-Red starts. If some error that closes the bot occours, a trigger is sent to my machine, generating an alarm.
+
+<p align="center">
+<img src="./imgs/05.PNG" width=40% height=5% align="center">
+    <br />Write node to write messages to txt file.
+</p>
+<br /><br />
+
+Note that the append option is choose, this way is possible to create a 'buffer' of messages to be send in order.
+
+## IMPROVEMENTS
+The next step is send images and documents files. I'll create it soon. 
+<br />Some codes may not be the smartest way to be done, since I always try to use the fatest way that I know (check datefull() function).
---
+++ WhatsApp_Bot/bot_v1.js
@@ -0,0 +1,152 @@
+﻿//############################### START FUNCTION TIME ####################################
+function datefull(){
+    var currentdate = new Date(); 
+    //day
+    var day = currentdate.getDate();
+    if(day < 10 ){
+        day = "0" + day;
+    }
+    //month
+    var month = (currentdate.getMonth()+1);
+    if(month < 10 ){
+        month = "0" + month;
+    }
+    //hours
+    var hours = currentdate.getHours();
+    if(hours < 10 ){
+        hours = "0" + hours;
+    }
+    //minutes
+    var minutes = currentdate.getMinutes();
+    if(minutes < 10 ){
+        minutes = "0" + minutes;
+    }
+    //seconds
+    var seconds = currentdate.getSeconds();
+    if(seconds < 10 ){
+        seconds = "0" + seconds;
+    }
+    //year
+    var year = currentdate.getFullYear();
+    
+    var datetime = `${day}/${month}/${year} @ ${hours}:${minutes}:${seconds}`;
+    return datetime;
+    }
+    //############################### END FUNCTION TIME ####################################
+    
+    
+    //############################### START FUNCTION MSG ####################################
+    function msg(path,filename){
+        //read files
+        var fs = require('fs');
+        const { type } = require('os');
+        const internal = require('stream');
+        //variables
+        var text = "";
+        let newtext = "";
+        let firstline = "";
+        let final_text = [];
+        //read files
+        text = fs.readFileSync(path+"\\"+filename, 'utf8').toString().split("\n"); //read all lines of the file
+        firstline = text[0].split('\n').toString() + "\n";//getting only the firstline
+            text.slice(1).forEach(element => { //foreach to ignore the first line and save all others
+            newtext = newtext + element + "\n";
+        });
+        //remove the used line
+        fs.writeFile(path+"\\"+filename, newtext, (err) => { //write the new file, with the first line removed
+            // In case of a error throw err.
+            if (err) throw err;
+        })
+          final_text[0] = firstline.substring(0,firstline.indexOf(' ')); //numbers
+          final_text[1] = firstline.substring(firstline.indexOf(' ') + 1); //text
+        return final_text;
+        }
+    //############################### END FUNCTION MSG ####################################
+    
+    
+    //############################### START FUNCTION WHATSAPP ####################################
+    function wpp(path,filename,delay_message=5000,delay_read=1000,max_error_tries=3){
+    //code start
+    console.log("Starting Code...");
+    const args = process.argv;
+    const qrcode = require('qrcode-terminal');
+    const { Client, LocalAuth } = require('whatsapp-web.js');
+    console.log("Auth ok...");
+    
+    let repeat = true;
+    let i = 0;
+    
+    while(repeat == true){
+        try{ //start try
+                    repeat = false; //stop while loop
+                    //auth
+                    const client = new Client({
+                        authStrategy: new LocalAuth()
+                    });
+                    console.log("New client OK...");
+                    //qr code
+                    client.on('qr', qr => {
+                        qrcode.generate(qr, {small: true});
+                    });
+                    console.log("QR code OK...");
+    
+                    //client ready
+                    client.on('ready', () => {
+                        console.log('Client is ready!');
+    
+                    //send msg
+                    time=setInterval(function(){
+                        let returning = msg(path,filename);
+
+                    //console.log(numbers);
+                    //console.log(text);
+                    if(returning[0].length > 10){
+                    const numbers = returning[0].split(",");
+                    let text = returning[1];
+                    console.log(`Message Found on first Line - ${datefull()}.`);
+                    let rename = new RegExp("xxx", "g"); 
+                    text = text.replace(rename, '\n'); //replace all "xxx" to breakline "\n"
+                    let rename2 = new RegExp("_", "g");
+                    text = text.replace(rename2, ' '); //replace all "_" to " "
+                    let chatId = "";
+                    let j = 0;
+                    console.log(returning);
+                    numbers.forEach(element => {
+                           setTimeout(function() {
+                            chatId = element + "@c.us";
+                            client.sendMessage(chatId, text);
+                            console.log(`Message Sent to ${element} - ${datefull()}.`);
+                           }, delay_message * j);
+                            j++;   
+                    });
+                    }
+                    else{
+                        //console.log(`First line has empty message - ${datefull()}.`);
+                    }
+                    },delay_read);
+    
+                    });
+                    client.initialize();
+    
+            } //end try
+        catch{ //start catch
+            repeat = true; //repeate while loop
+            if(i++ == max_error_tries){ 
+                let errorcode = 1;
+                console.log(errorcode);
+                return errorcode;    
+                }
+            } //end catch
+    }
+    }
+    //############################### END FUNCTION WHATSAPP ####################################
+    
+    //---------
+    //START CONFIG
+    const path = "";
+    const filename = "";
+    //END CONFIG
+    //---------
+    
+    //call function wpp
+    wpp(path,filename,delay_read=10000,delay_message=5000);
